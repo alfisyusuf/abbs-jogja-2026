93c1009568dd7871b6087bbea5aade1fec1ac3ef
Alfi Suryani Yusuf <seladuakhsatrya@gmail.com>
2026-08-02 20:57:24 +0700

    perbaikan css

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abbs-jogja-2026.pages.dev',
   vite: {
     plugins: [tailwindcss()]
   },
